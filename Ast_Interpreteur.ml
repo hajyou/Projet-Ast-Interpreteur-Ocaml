fec689332c6404e1bb1b76e047897ef95abcef84
Youssef Haj <youssefhajri83@gmail.com>
2026-02-22 16:04:46 +0100

    Correction de bug

--- Ast_Interpreteur.ml
+++ Ast_Interpreteur.ml
@@ -49,6 +49,10 @@ type op_bin =
     | Fois
     | Div
 
+type op_bin_log =
+    | Et
+    | Ou
+
 type op_cmp =
     | Eq
     | Sup
@@ -69,15 +73,16 @@ type t_exp_bcl =
 type expr_log_eq =
     | Cst of est_log
     | Opun of op_un * expr_log_eq
-    | Opbin of op_bin * expr_log_eq * expr_log_eq
-    | Opcmp of op_cmp * expr_log_eq * expr_log_eq
+    | Opbin of op_bin_log * expr_log_eq * expr_log_eq
+    | Opcmp of op_cmp * ea_var * ea_var
 
 let string_of_cmp op =
     match op with
-    |Eq -> "=="
-    |gt -> ">"
+    | Eq -> "=="
+    | Sup -> ">"
+    | Inf -> "<"
 
-let interp_opbin op b1 b2 =
+let interp_opbin_log op b1 b2 =
     match op with
     | Et -> b1 && b2
     | Ou -> b1 || b2
@@ -85,32 +90,21 @@ let interp_opbin op b1 b2 =
 let interp_opcmp op n1 n2 =
     match op with
     | Eq -> n1 = n2
-    | gt -> n1 > n2
+    | Sup -> n1 > n2
+    | Inf -> n1 < n2
 
-let interp_ea_var env e =
+let rec interp_ea_var (env : (t_ident * int) list) (e : ea_var) : int =
     match e with
     | Cst n -> n
-    | Var id -> failwith ("Variable " ^ id ^ " non définie")
-    | Opbin (op, e1, e2) -> interp_opbin op (interp_ea_var env e1) (interp_ea_var env e2)
-    
-let rec string_of_op_expr_log_eq e =
-    match e with
-    | Cst Vrai -> "vrai"
-    | Cst Faux -> "faux"
-    | Opun(Non, e1) -> "non(" ^ (string_of_op_expr_log_eq e1) ^ ")"
-    | Opbin(op, e1, e2) -> "(" ^ (string_of_op_expr_log_eq e1) ^ (string_of_opbin op) ^ (string_of_op_expr_log_eq e2) ^")"
-    | Opcmp(op, e1, e2) -> "(" ^ (string_of_ea_var e1)  ^ (string_of_cmp op) ^ (string_of_ea_var e2) ^")"
-
-
-
-let rec interp_expr_log_eq e =
-    match e with
-    | Vrai -> true
-    | Faux -> false
-    | Opun(Non, e1) -> not(interp_expr_log_eq e1)
-    | Opbin(op, e1, e2) -> interp_opbin op(interp_expr_log_eq e1)(interp_expr_log_eq e2)
-    | Opcmp(op, ea1, ea2) -> interp_opcmp op(interp_ea_var env ea1)(interp_ea_var env ea2)
-
+    | Var id -> (try List.assoc id env with Not_found -> failwith ("Variable " ^ id ^ " non définie"))
+    | Opbin (op, e1, e2) ->
+        let n1 = interp_ea_var env e1 in
+        let n2 = interp_ea_var env e2 in
+        (match op with
+        | Plus -> n1 + n2
+        | Moins -> n1 - n2
+        | Fois -> n1 * n2
+        | Div -> n1 / n2)
 
 let afficher_opbin (op : op_bin) : string =
     match op with
@@ -126,6 +120,26 @@ let rec afficher_ea_var (e : ea_var) : string =
     | Opbin (op, e1, e2) ->
         "(" ^ (afficher_ea_var e1) ^ " " ^ (afficher_opbin op) ^ " " ^ (afficher_ea_var e2) ^ ")"
 
+let string_of_opbin_log op =
+    match op with
+    | Et -> " and "
+    | Ou -> " or "
+
+let rec string_of_op_expr_log_eq e =
+    match e with
+    | Cst Vrai -> "vrai"
+    | Cst Faux -> "faux"
+    | Opun(Non, e1) -> "non(" ^ (string_of_op_expr_log_eq e1) ^ ")"
+    | Opbin(op, e1, e2) -> "(" ^ (string_of_op_expr_log_eq e1) ^ (string_of_opbin_log op) ^ (string_of_op_expr_log_eq e2) ^")"
+    | Opcmp(op, e1, e2) -> "(" ^ (afficher_ea_var e1)  ^ (string_of_cmp op) ^ (afficher_ea_var e2) ^")"
+
+let rec interp_expr_log_eq env e =
+    match e with
+    | Cst Vrai -> true
+    | Cst Faux -> false
+    | Opun(Non, e1) -> not(interp_expr_log_eq env e1)
+    | Opbin(op, e1, e2) -> interp_opbin_log op (interp_expr_log_eq env e1) (interp_expr_log_eq env e2)
+    | Opcmp(op, ea1, ea2) -> interp_opcmp op (interp_ea_var env ea1) (interp_ea_var env ea2)
 
 let rec afficher_exp_bcl (exp : t_exp_bcl) : string =
     match exp with
@@ -140,6 +154,6 @@ let rec afficher_exp_bcl (exp : t_exp_bcl) : string =
 let x = Define ("x", Cst 5);;
 let y = Define ("y", Cst 37);;
 let z = Define ("z", Opbin (Plus, Var "x", Var "y"));;
-let seq = Sequence (x, Sequence (y, z));;  
+let seq = Sequence (x, Sequence (y, z));;
 
-let afficher = print_endline(afficher_exp_bcl seq) ;;
+let () = print_endline(afficher_exp_bcl seq)
